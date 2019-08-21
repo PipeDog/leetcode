9e77478d454187294bd8eded0fe8a2b32d527770
liang <18811130435@163.com>
2019-08-21 17:50:08 +0800

    commit

---
+++ 2.两数相加.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=golang
+ *
+ * [2] 两数相加
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	ret := &ListNode{}
+	curNode := ret
+	eachSum := 0
+
+	for l1 != nil || l2 != nil || eachSum > 0 {
+		if l1 != nil {
+			eachSum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			eachSum += l2.Val
+			l2 = l2.Next
+		}
+
+		curNode.Next = &ListNode{Val: eachSum % 10}
+		curNode = curNode.Next
+		eachSum /= 10
+	}
+
+	return ret.Next
+}
+
+
