6f0ae3277b9cd69717cbf35f99eb48ab7514ef89
liang <18811130435@163.com>
2019-08-22 15:21:29 +0800

    commit

---
+++ 3.无重复字符的最长子串.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+func lengthOfLongestSubstring(s string) int {
+    if len(s) <= 1 {
+		return len(s)
+	}
+
+	locs := make(map[string]int)
+	curLen := 0
+	maxLen := 0
+
+	for i := 0; i < len(s); i++ {
+		ch := s[i:i+1]
+		preLoc, ok := locs[ch]
+
+		if !ok || i - preLoc > curLen {
+			curLen++
+		} else {
+			if curLen > maxLen {
+				maxLen = curLen
+			}
+			curLen = i - preLoc
+		}
+
+		locs[ch] = i
+	}
+	
+	if curLen > maxLen {
+		maxLen = curLen
+	}
+
+	return maxLen
+}
+
