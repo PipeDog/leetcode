fcf90f8f50ce070904a12d5f5262afc3a3c9655d
liang <18811130435@163.com>
2019-08-27 09:57:15 +0800

    commit

---
+++ 22.括号生成.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=golang
+ *
+ * [22] 括号生成
+ */
+
+// https://leetcode-cn.com/problems/generate-parentheses/
+func generateParenthesis(n int) []string {
+	var isValidString func(string) bool
+
+	isValidString = func(str string) bool {
+		if len(str) % 2 != 0 {
+			return false
+		}
+
+		stack := ""
+		strLen := len(str)
+
+		for i := 0; i < strLen; i++ {
+			curChar := str[i: i+1]
+
+			// push stack
+			if curChar == "(" {
+				stack += curChar
+				continue
+			}
+
+			// pop stack
+			if curChar == ")" {
+				if len(stack) == 0 {
+					return false
+				}
+
+				if stack[len(stack)-1:] == "(" {
+					stack = stack[: len(stack)-1]
+				} else {
+					return false
+				}
+				continue
+			}
+
+			return false
+		}
+
+		return (len(stack) == 0)
+	}
+
+	ret := make([]string, 0)
+	filter := make(map[string]bool)
+
+	// 当前的str
+	// 总长度
+	// 当前位置
+	var combine func(string, int, int)
+
+	combine = func(str string, length, loc int) {
+		if loc >= length {
+			if isValidString(str) {
+				if _, ok := filter[str]; !ok {
+					filter[str] = true
+					ret = append(ret, str)
+				}
+			}
+			return
+		}
+
+		for i := 0; i < 2; i++ {
+			if i == 0 {
+				str += "("
+			} else {
+				str += ")"
+			}
+			combine(str, length, loc + 1)
+			str = str[: len(str)-1]
+		}
+	}
+
+	combine("(", n * 2, 1)
+	return ret
+}
+
