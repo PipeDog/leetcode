a937c6b319dde87ddd35e3f59ddac2ef3059a180
liang <18811130435@163.com>
2019-09-02 11:48:58 +0800

    commit

---
+++ 94.二叉树的中序遍历.go
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	ret := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	stack = append(stack, root)
+	
+	for len(stack) > 0 {
+		for stack[len(stack)-1].Left != nil {
+			left := stack[len(stack)-1].Left
+			stack = append(stack, left)
+		}
+
+		for len(stack) > 0 {
+			pop := stack[len(stack)-1]
+			ret = append(ret, pop.Val)
+			stack = stack[:len(stack)-1]
+
+			if pop.Right != nil {
+				stack = append(stack, pop.Right)
+				break
+			}
+		}
+	}
+
+	return ret
+}
+
