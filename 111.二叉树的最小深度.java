1cdfa82e8bb94e9df518d7ebaabb55c8b8fe5f16
liang <leiliang4@xdf.cn>
2022-03-18 15:49:44 +0800

    111. 二叉树的最小深度

---
+++ 111.二叉树的最小深度.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=111 lang=java
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (49.70%)
+ * Likes:    693
+ * Dislikes: 0
+ * Total Accepted:    348.5K
+ * Total Submissions: 700K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ * 
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 
+ * 说明：叶子节点是指没有子节点的节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数的范围在 [0, 10^5] 内
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    // BFS
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root); 
+        int depth = 1;
+
+        while (!queue.isEmpty()) {
+            List<TreeNode> list = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                list.add(queue.poll());
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                TreeNode node = list.get(i);
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
+
+}
+// @lc code=end
+
